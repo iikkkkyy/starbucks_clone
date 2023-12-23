6b2f563d15810ca1caf4f5decad0b1a7be230bb5
kimkangtae <tt997768@gmail.com>
2023-12-23 10:26:14 +0900

    테스트용 스크린 2,3 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:starbucks_clone/starbucks_main_screen.dart';
+import 'package:starbucks_clone/starbucks_main_screen2.dart';
+import 'package:starbucks_clone/starbucks_main_screen3.dart';
 
 void main() {
   runApp(const MyApp());
